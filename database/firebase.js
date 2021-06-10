4cf38c63c8de52f3f60f401d919ab31595fa68af
Camilo Villalobos <camilo.svs@gmail.com>
2021-06-10 19:10:00 -0400

    Clean api key

--- database/firebase.js
+++ database/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDtbOZJvwTb1_p2TfILoFcYsylmCfMQ688",
-    authDomain: "ontaapp-1827a.firebaseapp.com",
-    projectId: "ontaapp-1827a",
-    storageBucket: "ontaapp-1827a.appspot.com",
-    messagingSenderId: "845805564912",
-    appId: "1:845805564912:web:f4e7a6ddeda9460e27e335",
-    measurementId: "G-NP92JRZ41P"
+  //INSERT API KEY
 };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
